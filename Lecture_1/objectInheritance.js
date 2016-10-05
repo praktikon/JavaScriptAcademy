f8b41564ea833a7d259ef0cc76d0ece6734229b9
Yevhen Verba <verba_e_k@mail.ru>
2016-10-05 13:00:53 +0300

    enhancement :godmode:

--- Lecture_1/objectInheritance.js
+++ Lecture_1/objectInheritance.js
@@ -1,77 +1,90 @@
+/*
+1. Создать сущности Dog, Cat, Woodpecker, которые являются наследниками сущности Animal. 
+Animal содержит свойства age, name, sound, region и метод say. Dog, Cat, Woodpecker не содержит явно данных свойств, но наследует их у Animal. 
+Также они содержат метод goAway. Задание должно быть реализовано каждым из следующих способов: 
+    1 - прототипное наследование через функции-конструкторы 
+    2 - наследование через конструкцию Object.create()
+
+2. Вызвать метод say на каждой из сущностей.
+
+3. Реализовать функции getType(), которая принимает один из объектов Dog, Cat, Woodpecker и возвращает его тип не используя оператор instanceof,
+ а проверяя наличие свойств/методов объектов.
+
+4. Модифицировать функцию так, что она не принимает объект, а оперирует с объектом this. 
+Функция объявляется вне контекста, но вызывается на определенном объекте при помощи call/apply/bind.
+*/
+
 var Animal = {
-  constructor: function(age, name, sound, region){
-  this.age = age;
-  this.name = name;
-  this.sound = sound;
-  this.region = region;
-    return this;
- },
-  say: function(){
-    console.log(this.sound); 
-}
+    constructor: function(age, name, sound, region){
+    this.age = age;
+    this.name = name;
+    this.sound = sound;
+    this.region = region;
+        return this;
+    },
+    say: function(){
+        console.log(this.sound); 
+	}
 };
-anml = Object.create(Animal).constructor(4, 'anny', 'moo', 'region0');
 
 var Dog = Object.create(Animal);
-Dog.constructor = function(age, name, region){
-  Animal.constructor.call(this, age, name, 'woof', region);
-  return this;
+Dog.constructor = function(age, name, region) {
+    Animal.constructor.call(this, age, name, 'woof', region);
+    return this;
 }
 Dog.goAway = function() {
-  console.log('Go away! ' + this.sound);
+    console.log('Go away! ' + this.sound);
 };
-tusik = Object.create(Dog).constructor(2, 'tuz', 'region1');
-
-
 
 var Cat = Object.create(Animal);
-Cat.constructor = function(age, name, region){
-  Animal.constructor.call(this, age, name, 'meaw', region);
-  return this;
+Cat.constructor = function(age, name, region) {
+    Animal.constructor.call(this, age, name, 'meaw', region);
+    return this;
 }
 Cat.goAway = function() {
 	console.log('Go away! ' + this.sound);
 };
-kitty = Object.create(Cat).constructor(3, 'matroskin', 'region2');
-
 
 var Woodpecker = Object.create(Animal);
-Woodpecker.constructor = function(age, name, region){
-  Animal.constructor.call(this, age, name, 'tuk', region);
-  return this;
+Woodpecker.constructor = function(age, name, region) {
+    Animal.constructor.call(this, age, name, 'tuk', region);
+    return this;
 }
 Woodpecker.goAway = function() {
 	console.log('Go away! ' + this.sound);
 };
+
+tusik = Object.create(Dog).constructor(2, 'tuz', 'region1');
+kitty = Object.create(Cat).constructor(3, 'matroskin', 'region2');
 woody = Object.create(Woodpecker).constructor(3, 'dyatel', 'region3');
+anml = Object.create(Animal).constructor(4, 'anny', 'moo', 'region0');
 
-
-function getTypep(ob){
-	if (ob.__proto__.hasOwnProperty('goAway')){
+function getTypep(ob) {
+	if (Object.getPrototypeOf(ob).hasOwnProperty('goAway')) {
 		switch(ob.sound){
 			case 'woof': return 'Dog'; 
 			case 'meaw': return 'Cat';
 			case 'tuk' : return 'Woodpecker';
 			default: return 'something is wrong!';
-		};
-	}else if (ob.__proto__.hasOwnProperty('say')){
+		}
+	} else if (Object.getPrototypeOf(ob).hasOwnProperty('say')) {
 		return 'Animal';
-	}else {
+	} else {
 		return 'Not our type!';
 	}
 }
 
 function getTypepThis(){
-	if (this.__proto__.hasOwnProperty('goAway')){
-		switch(this.sound){
+	if (Object.getPrototypeOf(this).hasOwnProperty('goAway')) {
+		switch(this.sound) {
 			case 'woof': return 'Dog'; 
 			case 'meaw': return 'Cat';
 			case 'tuk' : return 'Woodpecker';
 			default: return 'something is wrong!';
-		};
-	}else if (this.__proto__.hasOwnProperty('say')){
+		}
+	} else if (Object.getPrototypeOf(this).hasOwnProperty('say')) {
 		return 'Animal';
-	}else {
+	} else {
 		return 'Not our type!';
 	}
 }
